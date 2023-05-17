1108fb90ae70081b0aeaf52c56f117306a2a5642
rgvieira <yami.hiei87@gmail.com>
2023-05-16 22:42:52 -0300

    MirageJS and Axios added to project

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Desenvolvimento de website',
+          ammount: 4500,
+          type: 'deposit',
+          category: 'Other Earnings',
+          createdAt: new Date()
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          ammount: 850,
+          type: 'withdraw',
+          category: 'Home',
+          createdAt: new Date()
+        }
+      ];
+    })
+  }
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
