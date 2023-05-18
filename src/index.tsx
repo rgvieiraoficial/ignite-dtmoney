9e46709da050e74e2b1451b416385eb0afc13b7f
rgvieira <yami.hiei87@gmail.com>
2023-05-18 20:57:41 -0300

    Finishied lesson inserindo transacao na api

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
+
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Desenvolvimento de website',
-          ammount: 4500,
-          type: 'deposit',
-          category: 'Other Earnings',
-          createdAt: new Date()
-        },
-        {
-          id: 2,
-          title: 'Aluguel',
-          ammount: 850,
-          type: 'withdraw',
-          category: 'Home',
-          createdAt: new Date()
-        }
-      ];
+      return this.schema.all('transaction');
     })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
+    });
   }
 });
 
