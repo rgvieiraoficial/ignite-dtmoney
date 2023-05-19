c0befeaca3751c0fd7ae1e4db8bb043a09c729fe
rgvieira <yami.hiei87@gmail.com>
2023-05-18 22:09:58 -0300

    Finished lesson carregando transacoes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import Modal from 'react-modal';
 
-import { TransactionsContext } from './TransactionsContext';
+import { TransactionsProvider } from './TransactionsContext';
 
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
@@ -23,7 +23,7 @@ export function App() {
   }
 
   return (
-    <TransactionsContext.Provider value={[]}>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 
       <Dashboard />
@@ -34,6 +34,6 @@ export function App() {
       />
 
       <GlobalStyle />
-    </TransactionsContext.Provider>
+    </TransactionsProvider>
   );
 }
